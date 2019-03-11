eb1d12f01f354fd753497eabf4ed1c7a96460752
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-03-11 10:57:28 +0700

    lab  session2

---
+++ src/Lab_session2/Main.java
@@ -0,0 +1,13 @@
+package Lab_session2;
+
+public class Main {
+
+    public static void main(String[] args){
+        Nhanvien nhanvien1 = new Nhanvien();
+        nhanvien1.inputInfo();
+        nhanvien1.printInfo();
+        System.out.println("tien thuong cua nhan vien: "+nhanvien1.tinhThuong());
+        Nhanvien nhanvien2 = new Nhanvien();
+    }
+
+}
