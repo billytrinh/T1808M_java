b60544dcc7d8df0d8870ecb29f95d65d022c9fd0
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-03-13 08:53:17 +0700

    constructors - getter/setter func

--- src/Lab_session2/Main.java
+++ src/Lab_session2/Main.java
@@ -4,10 +4,11 @@ public class Main {
 
     public static void main(String[] args){
         Nhanvien nhanvien1 = new Nhanvien();
-        nhanvien1.inputInfo();
-        nhanvien1.printInfo();
-        System.out.println("tien thuong cua nhan vien: "+nhanvien1.tinhThuong());
-        Nhanvien nhanvien2 = new Nhanvien();
+        //nhanvien1.inputInfo();
+       // nhanvien1.printInfo();
+
+        Nhanvien nhanvien2 = new Nhanvien(nhanvien1.getName(),nhanvien1.age,nhanvien1.address
+                ,nhanvien1.salary,nhanvien1.workingTime);
     }
 
 }
--- src/Lab_session2/Nhanvien.java
+++ src/Lab_session2/Nhanvien.java
@@ -10,13 +10,66 @@ public class Nhanvien {
     public double salary;
     public int workingTime;
 
+    public Nhanvien() {
+    }
+
+    public Nhanvien(String name, int age, String address, double salary, int workingTime) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.salary = salary;
+        this.workingTime = workingTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getWorkingTime() {
+        return workingTime;
+    }
+
+    public void setWorkingTime(int workingTime) {
+        this.workingTime = workingTime;
+    }
+
     public void inputInfo(){
         System.out.println("Nhap ten nhan vien:");
         Scanner scanner = new Scanner(System.in);
-        name = scanner.nextLine();
+        this.setName(scanner.nextLine());
 
         System.out.println("nhap tuoi:");
-        age = scanner.nextInt();
+
+        this.setAge(scanner.hasNextInt()?scanner.nextInt():18);
+
         scanner.nextLine();
 
         System.out.println("nhap dia chi:");
@@ -31,7 +84,7 @@ public class Nhanvien {
 
     public void printInfo(){
         System.out.println("Thong tin nhan vien:");
-        System.out.println(name+" - tuoi:"+age);
+        System.out.println(this.getName()+" - tuoi:"+age);
     }
 
     public double tinhThuong(){
