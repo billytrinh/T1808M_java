5212fc0bb4d1bd6879086bd41ca4784c384cde8b
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-05-03 10:33:13 +0700

    timezone

--- src/JV2_Session9/Main.java
+++ src/JV2_Session9/Main.java
@@ -1,17 +1,24 @@
 package JV2_Session9;
 
 import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Main {
     public static void main(String args[]){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+
+        LocalDateTime localDateTime1 = LocalDateTime.now(ZoneId.of("America/Los_Angeles"));
+        System.out.println(localDateTime1);
+
         Locale vi = new Locale("vi","VN");
         Locale.setDefault(vi);
 
         ResourceBundle resourceBundle = ResourceBundle
                 .getBundle("JV2_Session9.Message");
-
 
         System.out.println(resourceBundle.getString("hello"));
 
